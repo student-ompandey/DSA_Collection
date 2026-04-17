516bb07441f89cc6b563960787a74c1cc96819ae
Om Pandey <ompandey628782@gmail.com>
2026-04-17 10:47:55 +0530

    array

---
+++ TNP/Array.java
@@ -0,0 +1,29 @@
+public class Array {
+    public static void main (String [] args){
+        int arr [] = {1, 2, 3, 4};
+
+        // // sum 
+        // int sum = 0;
+        // for(int i=0;i<arr.length;i++){
+        //     sum += arr[i];
+        // }
+        // System.out.println(sum);
+
+        // // avarege 
+        // System.out.println(sum/arr.length);
+
+        // //
+
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]%2==0){
+        //         System.out.println("Even : " + arr[i]);
+        //     } else {
+        //         System.out.println("Odd : " + arr[i]);
+        //     }
+        // }
+
+
+        
+
+    }
+}
